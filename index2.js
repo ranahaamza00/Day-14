33cbe31464fd3d1e23a740195f5ef338d0251560
ranahaamza00 <ranahaamza00@gmail.com>
2024-05-18 18:27:15 +0500

    first commit

---
+++ index2.js
@@ -0,0 +1,15 @@
+var magicians = ["Alice", "David", "Chris"];
+function show_magicians(magicians) {
+    magicians.forEach(function (magician) {
+        console.log(magician);
+    });
+}
+show_magicians(magicians);
+//Qno : 42
+function make_great(magicians) {
+    for (var i = 0; i < magicians.length; i++) {
+        magicians[i] = magicians[i] + " the Great";
+    }
+}
+make_great(magicians);
+show_magicians(magicians);
